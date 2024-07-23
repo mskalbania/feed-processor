ec4f110a28185d17d21b304e209baec08fa9e16b
mskalbania <mateusz.skalbania1@tesco.com>
2024-07-23 11:13:47 +0200

    Repo structure

---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func init() {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
+
+func main() {
+	log.Println("Starting application...")
+}
